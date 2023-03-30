c8c3aef1143aed0448f81db07547a419b8a55b54
funemy <liyzunique@gmail.com>
2023-03-30 13:25:53 -0700

    an attempt for alpha equiv, and decided to try out fourmolu

--- src/Lang.hs
+++ src/Lang.hs
@@ -1,72 +1,75 @@
-module Lang (Name(..), Term(..)) where
-import           Text.Printf (printf)
+module Lang (Name (..), Term (..)) where
+
+import Text.Printf (printf)
 
 newtype Name = Name String
-  deriving (Eq)
+    deriving (Eq)
 
 instance Show Name where
-  show (Name s) = s
+    show (Name s) = s
 
 -- TODO: add List and Vec
 -- TODO: add Either
 data Term
-  = Var Name
-  | Pi Name Term Term
-  | Lam Name Term
-  | App Term Term
-  | Sigma Name Term Term
-  -- Pair constructor
-  | MkPair Term Term
-  | Fst Term
-  | Snd Term
-  | Nat
-  | Zero
-  | Succ Term
-  | IndNat Term Term Term Term
-  -- Equality type
-  | Equal Term Term
-  -- Ctor for Equality types (i.e., refl)
-  | Refl
-  -- Substitution on equal terms
-  -- FIXME: missing sym/cong/trans
-  | Subst Term Term Term
-  | UnitTy
-  | Unit
-  | Absurd
-  | IndAbsrud Term Term
-  -- Atom type as in "The Litte Typer"
-  | Atom
-  -- Ctor for Atom
-  | Quote String
-  | Universe
-  | Isa Term Term
+    = Var Name
+    | Pi Name Term Term
+    | Lam Name Term
+    | App Term Term
+    | Sigma Name Term Term
+    | -- Pair constructor
+      MkPair Term Term
+    | Fst Term
+    | Snd Term
+    | Nat
+    | Zero
+    | Succ Term
+    | IndNat Term Term Term Term
+    | -- Equality type
+      Equal Term Term
+    | -- Ctor for Equality types (i.e., refl)
+      -- I'm curious about this, isn't `refl` in Agda takes implicit arguments?
+      Refl
+    | -- Substitution on equal terms
+      -- FIXME: missing sym/cong/trans
+      Subst Term Term Term
+    | UnitTy
+    | Unit
+    | Absurd
+    | IndAbsrud Term Term
+    | -- Atom type as in "The Litte Typer"
+      Atom
+    | -- Ctor for Atom
+      Quote String
+    | Universe
+    | -- Type annotation (ascription)
+      As Term Term
 
 toInt :: Term -> Int
-toInt Zero     = 0
+toInt Zero = 0
 toInt (Succ x) = 1 + toInt x
-toInt _        = error "Cannot print ill-formed natural numbers."
+toInt _ = error "Cannot print ill-formed natural numbers."
 
 instance Show Term where
-  show (Var s)                 = show s
-  show (Pi n ty ty')           = printf "Π%s:%s.%s" (show n) (show ty) (show ty')
-  show (Lam n b)               = printf "λ%s.%s" (show n) (show b)
-  show (App f a)               = printf "%s %s" (show f) (show a)
-  show (Sigma n ty ty')        = printf "Σ%s:%s.%s" (show n) (show ty) (show ty')
-  show (MkPair l r)            = printf "(%s, %s)" (show l) (show r)
-  show (Fst p)                 = printf "%s.1" (show p)
-  show (Snd p)                 = printf "%s.2" (show p)
-  show Nat                     = "Nat"
-  show Zero                    = "0"
-  show n@(Succ _)              = show $ toInt n
-  show (IndNat ty t1 t2 t3)    = printf "(ind-nat %s %s %s %s)" (show ty) (show t1) (show t2) (show t3)
-  show (Equal ty ty')          = printf "%s≡%s" (show ty) (show ty')
-  show Refl                    = "refl"
-  show (Subst t1 t2 t3)        = printf "(subst %s %s %s)" (show t1) (show t2) (show t3)
-  show UnitTy                  = "Unit"
-  show Unit                    = "()"
-  show Absurd                  = "⊥"
-  show (IndAbsrud e ty)        = printf "(ind-absurd %s %s)" (show e) (show ty)
-  show Atom                    = "Atom"
-  show (Quote s)               = printf "'%s" s
-  show Universe                = "U"
-  show (Isa e ty)              = printf "(isa %s %s)" (show e) (show ty)
+    show (Var s) = show s
+    show (Pi n ty ty') = printf "Π%s:%s.%s" (show n) (show ty) (show ty')
+    show (Lam n b) = printf "λ%s.%s" (show n) (show b)
+    show (App f a) = printf "%s %s" (show f) (show a)
+    show (Sigma n ty ty') = printf "Σ%s:%s.%s" (show n) (show ty) (show ty')
+    show (MkPair l r) = printf "(%s, %s)" (show l) (show r)
+    show (Fst p) = printf "%s.1" (show p)
+    show (Snd p) = printf "%s.2" (show p)
+    show Nat = "Nat"
+    show Zero = "0"
+    show n@(Succ _) = show $ toInt n
+    show (IndNat ty t1 t2 t3) = printf "(ind-nat %s %s %s %s)" (show ty) (show t1) (show t2) (show t3)
+    show (Equal ty ty') = printf "%s≡%s" (show ty) (show ty')
+    show Refl = "refl"
+    show (Subst t1 t2 t3) = printf "(subst %s %s %s)" (show t1) (show t2) (show t3)
+    show UnitTy = "Unit"
+    show Unit = "()"
+    show Absurd = "⊥"
+    show (IndAbsrud e ty) = printf "(ind-absurd %s %s)" (show e) (show ty)
+    show Atom = "Atom"
+    show (Quote s) = printf "'%s" s
+    show Universe = "U"
+    show (As e ty) = printf "(as %s %s)" (show e) (show ty)
---
+++ src/TyCheck.hs
@@ -0,0 +1,75 @@
+module TyCheck () where
+
+import Lang (Name, Term (..))
+
+type Depth = Int
+
+newtype NameSpace = NameSpace [(Name, Depth)]
+
+emptyNS :: NameSpace
+emptyNS = NameSpace []
+
+lookupNS :: Name -> NameSpace -> Maybe Int
+lookupNS n (NameSpace []) = Nothing
+lookupNS n (NameSpace ((n', d) : xs))
+    | n == n' = Just d
+    | otherwise = lookupNS n (NameSpace xs)
+
+extendNS :: Name -> Depth -> NameSpace -> NameSpace
+extendNS n d (NameSpace xs) = NameSpace ((n, d) : xs)
+
+-- the auxliary function for alpha equivalence checking
+alphaEquiv' :: Int -> NameSpace -> Term -> NameSpace -> Term -> Bool
+-- simple cases
+alphaEquiv' _ _ Nat _ Nat = True
+alphaEquiv' _ _ Zero _ Zero = True
+alphaEquiv' _ _ UnitTy _ UnitTy = True
+alphaEquiv' _ _ Unit _ Unit = True
+alphaEquiv' _ _ Absurd _ Absurd = True
+alphaEquiv' _ _ Atom _ Atom = True
+alphaEquiv' _ _ Universe _ Universe = True
+alphaEquiv' _ _ Refl _ Refl = True
+alphaEquiv' _ _ (Quote s1) _ (Quote s2) = s1 == s2
+alphaEquiv' _ ns1 (Var n1) ns2 (Var n2) = lookupNS n1 ns1 == lookupNS n2 ns2
+-- inductive cases
+alphaEquiv' d ns1 (App t11 t12) ns2 (App t21 t22) = alphaEquiv' d ns1 t11 ns2 t21 && alphaEquiv' d ns1 t12 ns2 t22
+alphaEquiv' d ns1 (MkPair t11 t12) ns2 (MkPair t21 t22) = alphaEquiv' d ns1 t11 ns2 t21 && alphaEquiv' d ns1 t12 ns2 t22
+alphaEquiv' d ns1 (Fst t1) ns2 (Fst t2) = alphaEquiv' d ns1 t1 ns2 t2
+alphaEquiv' d ns1 (Snd t1) ns2 (Snd t2) = alphaEquiv' d ns1 t1 ns2 t2
+alphaEquiv' d ns1 (Succ t1) ns2 (Succ t2) = alphaEquiv' d ns1 t1 ns2 t2
+alphaEquiv' d ns1 (IndAbsrud t11 t12) ns2 (IndAbsrud t21 t22) = alphaEquiv' d ns1 t11 ns2 t21 && alphaEquiv' d ns1 t12 ns2 t22
+alphaEquiv' d ns1 (As t11 t12) ns2 (As t21 t22) = alphaEquiv' d ns1 t11 ns2 t21 && alphaEquiv' d ns1 t12 ns2 t22
+alphaEquiv' d ns1 (Equal t11 t12) ns2 (Equal t21 t22) = alphaEquiv' d ns1 t11 ns2 t21 && alphaEquiv' d ns1 t12 ns2 t22
+alphaEquiv' d ns1 (Subst t11 t12 t13) ns2 (Subst t21 t22 t23) =
+    let equiv1 = alphaEquiv' d ns1 t11 ns2 t21
+        equiv2 = alphaEquiv' d ns1 t12 ns2 t22
+        equiv3 = alphaEquiv' d ns1 t13 ns2 t23
+     in equiv1 && equiv2 && equiv3
+alphaEquiv' d ns1 (IndNat t11 t12 t13 t14) ns2 (IndNat t21 t22 t23 t24) =
+    let equiv1 = alphaEquiv' d ns1 t11 ns2 t21
+        equiv2 = alphaEquiv' d ns1 t12 ns2 t22
+        equiv3 = alphaEquiv' d ns1 t13 ns2 t23
+        equiv4 = alphaEquiv' d ns1 t14 ns2 t24
+     in equiv1 && equiv2 && equiv3 && equiv4
+-- interesting cases
+alphaEquiv' d ns1 (Pi n1 t11 t12) ns2 (Pi n2 t21 t22) =
+    let equiv1 = alphaEquiv' d ns1 t11 ns2 t21
+        ns1' = extendNS n1 d ns1
+        ns2' = extendNS n2 d ns2
+        equiv2 = alphaEquiv' (d + 1) ns1' t12 ns2' t22
+     in equiv1 && equiv2
+alphaEquiv' d ns1 (Lam n1 t1) ns2 (Lam n2 t2) =
+    let ns1' = extendNS n1 d ns1
+        ns2' = extendNS n2 d ns2
+     in alphaEquiv' (d + 1) ns1' t1 ns2' t2
+alphaEquiv' d ns1 (Sigma n1 t11 t12) ns2 (Sigma n2 t21 t22) =
+    let equiv1 = alphaEquiv' d ns1 t11 ns2 t21
+        ns1' = extendNS n1 d ns1
+        ns2' = extendNS n2 d ns2
+        equiv2 = alphaEquiv' (d + 1) ns1' t12 ns2' t22
+     in equiv1 && equiv2
+-- fallback case
+alphaEquiv' _ _ _ _ _ = False
+
+alphaEquiv :: Term -> Term -> Bool
+alphaEquiv t1 t2 = alphaEquiv' 0 emptyNS t1 emptyNS t2
